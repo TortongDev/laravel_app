a5a8a4178420470afdca55e337bcf375ec8b13ba
tortongDev <kittithat.charoenkhet101@gmail.com>
2023-04-10 20:20:53 -0700

    test route get to view about page

--- laravel/routes/api.php
+++ laravel/routes/api.php
@@ -13,7 +13,9 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
-
+Route::get('/about',function(){
+    return view('about.blade.php');
+});
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
